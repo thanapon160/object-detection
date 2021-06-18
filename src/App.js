0a7924b89ce53075647204dbeec7dd135cba179f
Thanapon <thanapon160@hotmail.com>
2021-06-18 20:17:24 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -41,18 +41,6 @@ function App() {
   const rawData = data[0]?.raw_data
   console.log(detectedData)
 
-  const listData = [];
-  for (let i = 0; i < 23; i++) {
-    listData.push({
-      href: 'https://ant.design',
-      title: `ant design part ${i}`,
-      description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-      content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    });
-  }
-
   return (
     <div className="App">
       <div className='upload-container'>
