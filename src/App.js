fc3e2a023d79dc054d5e30a72e169ab76ec712a2
Thanapon <thanapon160@hotmail.com>
2021-06-18 20:16:01 +0700

    finish detet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import nvision from "@nipacloud/nvision/dist/browser/nvision"
 import { useState } from "react";
+import { Upload, List, Space } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+import "./css/file-up.css"
 
 require('dotenv').config();
+
+// antd components
+const { Dragger } = Upload;
 
 function App() {
   const [data, setData] = useState([])
@@ -17,7 +23,11 @@ function App() {
   });
 
   const handleUpload = async (e) => {
-    const res = await toBase64(e.target.files[0])
+    console.log(e)
+    console.log(e.file)
+    // console.log(e.target.files)
+    // const res = await toBase64(e.target.files[0])
+    const res = await toBase64(e.file.originFileObj)
     const imageBase64 = res.split(',')[1]
     await objectDetectionService.predict({
       rawData: imageBase64,
@@ -27,17 +37,64 @@ function App() {
       setData([result])
     });
   }
-  console.log(data)
+  const detectedData = data[0]?.detected_objects
   const rawData = data[0]?.raw_data
+  console.log(detectedData)
+
+  const listData = [];
+  for (let i = 0; i < 23; i++) {
+    listData.push({
+      href: 'https://ant.design',
+      title: `ant design part ${i}`,
+      description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+  }
 
   return (
     <div className="App">
-      {rawData && <img src={`data:image/jpeg;base64,${rawData}`}></img>}
-      <br/>
-      <label htmlFor="myfile">Select a file:</label>
-      <input type="file" id="myfile" onChange={handleUpload} />
-      {{data} && <p>{JSON.stringify(data)}</p>}
-
+      <div className='upload-container'>
+        <Dragger onChange={handleUpload} onDrop={handleUpload} showUploadList={false}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        </Dragger>
+      </div>
+      {rawData && <img src={`data:image/jpeg;base64,${rawData}`} className="image-container"></img>}
+      <br />
+      <List
+      className="list-container"
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: page => {
+            console.log(page);
+          },
+          pageSize: 5,
+        }}
+        dataSource={detectedData}
+        renderItem={item => (
+          <List.Item
+            key={item.name}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src={`data:image/jpeg;base64,${item.cropped_image}`}
+              />
+            }
+          >
+            <List.Item.Meta
+              title={item.name}
+              description=""
+            />
+            {item.parent}
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
